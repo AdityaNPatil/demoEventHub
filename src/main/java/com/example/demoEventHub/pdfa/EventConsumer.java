05d8ec9e8bd6e16d619f00fe3b3b583d33454d6f
AdityaNPatil <adityapatil2708@gmail.com>
2025-04-22 17:49:24 +0530

    temporary comment out

--- src/main/java/com/example/demoEventHub/pdfa/EventConsumer.java
+++ src/main/java/com/example/demoEventHub/pdfa/EventConsumer.java
@@ -1,33 +1,33 @@
-//package com.example.demoEventHub.pdfa;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.messaging.Message;
-//
-//import java.util.function.Consumer;
-//
-//@Configuration
-//@RequiredArgsConstructor
-//@Slf4j
-//public class EventConsumer {
-//    private static final Logger LOGGER = LoggerFactory.getLogger(EventConsumer.class);
-//    private final ObjectMapper objectMapper = new ObjectMapper();
-//
-//    @Bean
-//    public Consumer<Message<String>> consume() {
-//        LOGGER.info("PDF/A EVENT CONSUMER STARTED");
-//        return message -> {
-//            try {
-//                PdfaEvent event = objectMapper.readValue(message.getPayload(), PdfaEvent.class);
-//                LOGGER.info("Received PDF/A EventHub Message: JobId={}, Status={}, CustomerId={}", event.getConversionJobId(), event.getStatus(), event.getCustomerId());
-//            } catch (Exception e) {
-//                LOGGER.error("Error processing PDF/A message", e);
-//            }
-//        };
-//    }
-//}
+package com.example.demoEventHub.pdfa;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+
+import java.util.function.Consumer;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class EventConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventConsumer.class);
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Bean
+    public Consumer<Message<String>> consume() {
+        LOGGER.info("PDF/A EVENT CONSUMER STARTED");
+        return message -> {
+            try {
+                PdfaEvent event = objectMapper.readValue(message.getPayload(), PdfaEvent.class);
+                LOGGER.info("Received PDF/A EventHub Message: JobId={}, Status={}, CustomerId={}", event.getConversionJobId(), event.getStatus(), event.getCustomerId());
+            } catch (Exception e) {
+                LOGGER.error("Error processing PDF/A message", e);
+            }
+        };
+    }
+}
--- src/main/java/com/example/demoEventHub/pdfa/EventHubConsumerMessaging.java
+++ src/main/java/com/example/demoEventHub/pdfa/EventHubConsumerMessaging.java
@@ -1,16 +1,22 @@
-package com.example.demoEventHub.pdfa;
-
-import com.azure.spring.messaging.eventhubs.implementation.core.annotation.EventHubsListener;
-import org.springframework.stereotype.Service;
-
-@Service
-public class EventHubConsumerMessaging {
-    private static final String EVENT_HUB_NAME = "pdfa-eventhub";
-    private static final String CONSUMER_GROUP = "$DEFAULT";
-
-    @EventHubsListener(destination = EVENT_HUB_NAME, group = CONSUMER_GROUP)
-    public void handleMessageFromEventHub(String message) {
-        System.out.printf("New PDF/A EventHub message received: %s%n", message);
-    }
-
-}
+//package com.example.demoEventHub.pdfa;
+//
+//import com.azure.spring.messaging.eventhubs.implementation.core.annotation.EventHubsListener;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class EventHubConsumerMessaging {
+//    @Value("${pdfa.eventhub.name}")
+//    private final String eventHubName;
+//    @Value("${pdfa.eventhub.consumergroup}")
+//    private final String consumerGroup;
+//
+//    private static final String EVENT_HUB_NAME = "pdfa-eventhub";
+//    private static final String CONSUMER_GROUP = "$DEFAULT";
+//
+//    @EventHubsListener(destination = eventHubName, group = CONSUMER_GROUP)
+//    public void handleMessageFromEventHub(String message) {
+//        System.out.printf("New PDF/A EventHub message received: %s%n", message);
+//    }
+//
+//}
